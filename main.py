90381c29eeb0f5abe168683d4bf9101c0af44877
TinTooth <boconne4@gmail.com>
2022-09-16 11:32:29 -0500

    Added random_selector()

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 destinations_list = ["Milwaukee","Madison","Wisconsin Dells","Lake Geneva","Oconomowoc"]
 restaurants_list = ["Kopps","Culvers","Balistreri's", "Geneva ChopHouse", 
 "The Baker House", "OakFire", "The Keg & Patio", "River Walk Pup", "Ishnala Supper Club",
@@ -6,3 +7,7 @@ transportaion_list = ["Rental Car", "Uber", "Personal Vechicle", "Coach Bus"]
 entertainment_list = ["River Cruise", "Kayaking","Fishing","Brewer's Game", "Bucks Game", "Bar Hopping", "Water Park", 
 "National Parks", "Dell's Main Street", "Badger's Game"]
 
+def random_selector(list1):
+    return random.choice(list1)
+
+print(random_selector(destinations_list))
