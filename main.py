f360c7079916ebc9f132149fe1e137f564ea4b05
TinTooth <boconne4@gmail.com>
2022-09-16 11:22:32 -0500

    Added the four lists

---
+++ main.py
@@ -0,0 +1,8 @@
+destinations_list = ["Milwaukee","Madison","Wisconsin Dells","Lake Geneva","Oconomowoc"]
+restaurants_list = ["Kopps","Culvers","Balistreri's", "Geneva ChopHouse", 
+"The Baker House", "OakFire", "The Keg & Patio", "River Walk Pup", "Ishnala Supper Club",
+"AJ's Pub", "Mantra Indian Bistro" ]
+transportaion_list = ["Rental Car", "Uber", "Personal Vechicle", "Coach Bus"]
+entertainment_list = ["River Cruise", "Kayaking","Fishing","Brewer's Game", "Bucks Game", "Bar Hopping", "Water Park", 
+"National Parks", "Dell's Main Street", "Badger's Game"]
+
