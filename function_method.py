4c73ca1ce505ca0716e2b873d5c8b77c4626c242
TinTooth <boconne4@gmail.com>
2022-09-16 14:22:52 -0500

    Iterated and renamed/removed files

--- function_method.py
+++ function_method.py
@@ -2,13 +2,11 @@ import random
 
 def run_day_trip_generator():
     options_list = generate_trip_options()
-    current_trip = generate_trip(options_list)
+    current_trip = generate_random_trip(options_list)
     print_full_trip(current_trip)
-    satitisfied(current_trip,options_list)
+    finalize_trip(current_trip,options_list)
 
-      
-
-def satitisfied(current_trip,options_list):
+def finalize_trip(current_trip,options_list):
     complete = ""
     while complete !="Y":
         complete = input("Are you satisifed with your trip? Y or N? ")
@@ -16,26 +14,21 @@ def satitisfied(current_trip,options_list):
             current_trip = change_option(current_trip,options_list)
             print_full_trip(current_trip)
         else:
+            print()
             print("Thank You. Here is your finalized trip: ")
             print_full_trip(current_trip)
-
 
 def change_option(current_trip, options_list):
     change = input("Which would you like to change? 'Destination'  'Restaurant'  'Transportation' or 'Entertainment'   ")
     if change == "Destination":
-        current_trip[0] = random_selector(options_list[0])
+        current_trip[0] = random.choice(options_list[0])
     elif change == "Restaurant":
-        current_trip[1] = random_selector(options_list[1])
-    elif change == "Transportaion":
-        current_trip[2] = random_selector(options_list[2])   
+        current_trip[1] = random.choice(options_list[1])
+    elif change == "Transportation":
+        current_trip[2] = random.choice(options_list[2])   
     elif change == "Entertainment":
-        current_trip[3] = random_selector(options_list[3])
+        current_trip[3] = random.choice(options_list[3])
     return current_trip
-
-
-
-def random_selector(list1):
-    return random.choice(list1)
 
 def generate_trip_options():
     destinations_list = ["Milwaukee","Madison","Wisconsin Dells","Lake Geneva","Oconomowoc"]
@@ -45,22 +38,21 @@ def generate_trip_options():
     result_list = [destinations_list,restaurants_list,transportaion_list,entertainment_list]
     return result_list
 
-def generate_trip(options_list):
+def generate_random_trip(options_list):
     result_list =[]
-    result_list.append(random_selector(options_list[0]))
-    result_list.append(random_selector(options_list[1]))
-    result_list.append(random_selector(options_list[2]))
-    result_list.append(random_selector(options_list[3]))
+    result_list.append(random.choice(options_list[0]))
+    result_list.append(random.choice(options_list[1]))
+    result_list.append(random.choice(options_list[2]))
+    result_list.append(random.choice(options_list[3]))
     return result_list
 
 def print_full_trip(trip_list):
     print()
-    print(f"Destination:   {trip_list[0]}")
-    print(f"Restaurant:    {trip_list[1]}")
-    print(f"Transportaion: {trip_list[2]}")
-    print(f"Entertainment: {trip_list[3]}")
+    print(f"Destination:    {trip_list[0]}")
+    print(f"Restaurant:     {trip_list[1]}")
+    print(f"Transportation: {trip_list[2]}")
+    print(f"Entertainment:  {trip_list[3]}")
     print()
-
 
 run_day_trip_generator()
 
--- main.py
+++ main.py
@@ -1,42 +1,98 @@
+# import random
+# destinations_list = ["Milwaukee","Madison","Wisconsin Dells","Lake Geneva","Oconomowoc"]
+# restaurants_list = ["Kopps","Culvers","Balistreri's", "Geneva ChopHouse", 
+# "The Baker House", "OakFire", "The Keg & Patio", "River Walk Pup", "Ishnala Supper Club",
+# "AJ's Pub", "Mantra Indian Bistro" ]
+# transportaion_list = ["Rental Car", "Uber", "Personal Vechicle", "Bicyle", "E-Scooter","Walking"]
+# entertainment_list = ["River Cruise", "Kayaking","Fishing","Brewer's Game", "Bucks Game", "Bar Hopping", "Water Park", 
+# "National Parks", "Dell's Main Street", "Badger's Game"]
+
+# destination = random.choice(destinations_list)
+# restaurant = random.choice(restaurants_list)
+# transportaion = random.choice(transportaion_list)
+# entertainment = random.choice(entertainment_list)
+# complete = ""
+
+# while complete != "Yes":
+#     print()
+#     print("The follow is your potential Day Trip!")
+#     print(f"Destination:   {destination}")
+#     print(f"Restaurant:    {restaurant}")
+#     print(f"Transportaion: {transportaion}")
+#     print(f"Entertainment: {entertainment}")
+#     print()
+#     complete = input("Are you happy with this trip, and would like to complete the reservation? 'Yes' or 'No'?  ")
+#     if complete != "Yes":
+#         change = input("Which would you like to change? 'Destination'  'Restaurant'  'Transportation' or 'Entertainment'   ")
+#         if change == "Destination":
+#             destination = random.choice(destinations_list)
+#         elif change == "Restaurant":
+#             restaurant = random.choice(restaurants_list)
+#         elif change == "Transportaion":
+#             transportaion = random.choice(transportaion_list)   
+#         elif change == "Entertainment":
+#              entertainment = random.choice(entertainment_list)
+#     else:
+#         print("Thank you for selcting this day trip! Goodbye ")
+
 import random
-destinations_list = ["Milwaukee","Madison","Wisconsin Dells","Lake Geneva","Oconomowoc"]
-restaurants_list = ["Kopps","Culvers","Balistreri's", "Geneva ChopHouse", 
-"The Baker House", "OakFire", "The Keg & Patio", "River Walk Pup", "Ishnala Supper Club",
-"AJ's Pub", "Mantra Indian Bistro" ]
-transportaion_list = ["Rental Car", "Uber", "Personal Vechicle", "Bicyle", "E-Scooter","Walking"]
-entertainment_list = ["River Cruise", "Kayaking","Fishing","Brewer's Game", "Bucks Game", "Bar Hopping", "Water Park", 
-"National Parks", "Dell's Main Street", "Badger's Game"]
 
-def random_selector(list1):
-    return random.choice(list1)
+def run_day_trip_generator():
+    options_list = generate_trip_options()
+    current_trip = generate_random_trip(options_list)
+    print_full_trip(current_trip)
+    finalize_trip(current_trip,options_list)
 
-destination = random_selector(destinations_list)
-restaurant = random_selector(restaurants_list)
-transportaion = random_selector(transportaion_list)
-entertainment = random_selector(entertainment_list)
-complete = ""
+def finalize_trip(current_trip,options_list):
+    complete = ""
+    while complete !="Y":
+        complete = input("Are you satisifed with your trip? Y or N? ")
+        if complete != "Y":
+            current_trip = change_option(current_trip,options_list)
+            print_full_trip(current_trip)
+        else:
+            print()
+            print("Thank You. Here is your finalized trip: ")
+            print_full_trip(current_trip)
 
-while complete != "Yes":
+def change_option(current_trip, options_list):
+    change = input("Which would you like to change? 'Destination'  'Restaurant'  'Transportation' or 'Entertainment'   ")
+    if change == "Destination":
+        current_trip[0] = random.choice(options_list[0])
+    elif change == "Restaurant":
+        current_trip[1] = random.choice(options_list[1])
+    elif change == "Transportation":
+        current_trip[2] = random.choice(options_list[2])   
+    elif change == "Entertainment":
+        current_trip[3] = random.choice(options_list[3])
+    return current_trip
+
+def generate_trip_options():
+    destinations_list = ["Milwaukee","Madison","Wisconsin Dells","Lake Geneva","Oconomowoc"]
+    restaurants_list = ["Kopps","Culvers","Balistreri's", "Geneva ChopHouse",  "The Baker House", "OakFire", "The Keg & Patio", "River Walk Pup", "Ishnala Supper Club","AJ's Pub", "Mantra Indian Bistro" ]
+    transportaion_list = ["Rental Car", "Uber", "Personal Vechicle", "Bicyle", "E-Scooter","Walking"]
+    entertainment_list = ["River Cruise", "Kayaking","Fishing","Brewer's Game", "Bucks Game", "Bar Hopping", "Water Park", "National Parks", "Dell's Main Street", "Badger's Game"]
+    result_list = [destinations_list,restaurants_list,transportaion_list,entertainment_list]
+    return result_list
+
+def generate_random_trip(options_list):
+    result_list =[]
+    result_list.append(random.choice(options_list[0]))
+    result_list.append(random.choice(options_list[1]))
+    result_list.append(random.choice(options_list[2]))
+    result_list.append(random.choice(options_list[3]))
+    return result_list
+
+def print_full_trip(trip_list):
     print()
-    print("The follow is your potential Day Trip!")
-    print(f"Destination:   {destination}")
-    print(f"Restaurant:    {restaurant}")
-    print(f"Transportaion: {transportaion}")
-    print(f"Entertainment: {entertainment}")
+    print(f"Destination:    {trip_list[0]}")
+    print(f"Restaurant:     {trip_list[1]}")
+    print(f"Transportation: {trip_list[2]}")
+    print(f"Entertainment:  {trip_list[3]}")
     print()
-    complete = input("Are you happy with this trip, and would like to complete the reservation? 'Yes' or 'No'?  ")
-    if complete != "Yes":
-        change = input("Which would you like to change? 'Destination'  'Restaurant'  'Transportation' or 'Entertainment'   ")
-        if change == "Destination":
-            destination = random_selector(destinations_list)
-        elif change == "Restaurant":
-            restaurant = random_selector(restaurants_list)
-        elif change == "Transportaion":
-            transportaion = random_selector(transportaion_list)   
-        elif change == "Entertainment":
-             entertainment = random_selector(entertainment_list)
-    else:
-        print("Thank you for selcting this day trip! Goodbye ")
+
+run_day_trip_generator()
+
 
 
 
